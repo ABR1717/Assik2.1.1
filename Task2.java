34c537000cb064749f0a9036e037db3210a6152a
Abr 1717 <ibrahimnygyman268@gmail.com>
2024-12-18 22:11:24 +0500

    Add files via upload

---
+++ Task2.java
@@ -0,0 +1,14 @@
+public class Task2 {
+    public static void main(String[] args) {
+        String[] array=new String[5];
+        array[0]="Even";
+        array[1]="Odd";
+        array[2]="Even";
+        array[3]="Odd";
+        array[4]="Even";
+
+        for(int i=0;i<array.length;i++){
+            System.out.println(array[i]);
+        }
+    }
+}
